7fd81604e99e2c59f974d755369a5d5391d85a00
Abdul-Majeed20 <abdulmajeedbaloch315@gmail.com>
2025-08-09 09:51:12 +0500

    Dependencies issue fixed

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from 'tailwindcss'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/postcss'
 
 export default defineConfig({
   plugins: [react()],
-  css: {
+   css: {
     postcss: {
       plugins: [tailwindcss()],
     },
   }
-})
+});
